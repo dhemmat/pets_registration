ea59f34384ccb11ffd69dc51312ace6eba0e9edc
David R. Hemmat <dhemmat@gmail.com>
2013-06-04 15:12:24 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetsApp::Application.config.secret_token = '1d3a28206a73b8489af2fb9a529436630f2bf011e6991b2e7201b9272a5785bb99396f79069653901312348b19257c4fdb145ccdd55302054747d2d1791a457e'
