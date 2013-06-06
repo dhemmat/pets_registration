001edf1b85a9df9ceda29a38deb16c0a056e7734
David R. Hemmat <dhemmat@gmail.com>
2013-06-05 23:14:51 -0400

    fixed controller, forms saving correctly

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -12,14 +12,14 @@ class RecordsController < ApplicationController
   	@owner = Owner.new(params[:owner])
   	@pet = Array.new
   	params[:number_of_pets].to_i.times do |t|
-  		@pet[t] = Pet.new(params[:pet[t]]) 
+  		@pet[t] = Pet.new(params[:pet][t]) 
   	end
 
 	if (@owner.valid? && (@pet.all? { |t| t.valid? }))
 	  @owner.save
 	  @pet.each { |t| t.save}
   	  flash[:success] = "Data saved successfully"
-  	  redirect_to @user
+  	  redirect_to @user  #FIX THIS
     else
       render 'new'
     end
