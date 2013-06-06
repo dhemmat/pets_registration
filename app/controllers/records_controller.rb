e1804f80603b3a8cc3209c4123bceca47c647655
David R. Hemmat <dhemmat@gmail.com>
2013-06-06 02:20:54 -0400

    pet forms retain data on errors

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,19 +1,24 @@
 class RecordsController < ApplicationController
 
+  	
+
   def new
+	
 	@owner = Owner.new
 	@pet = Array.new
-	RecordsHelper::ALLOWED_PETS.times do |t| 
-		@pet[t] = Pet.new 
-	end
-
+	RecordsHelper::ALLOWED_PETS.times { |t| @pet[t] = Pet.new } 
+	@pet_storage = Array.new unless defined?(@pet_storage)  
+	#	
+    #	RecordsHelper::ALLOWED_PETS.times { |t| @pet_storage[t] = {name:'', age:'', pet_type_id:''}}
+	#end
   end
   
   def create
   	@owner = Owner.new(params[:owner])
   	@pet = Array.new
   	params[:number_of_pets].to_i.times do |t|
-  		@pet[t] = Pet.new(params[:pet][t]) 
+  		@pet[t] = Pet.new(params[:pet][t])
+  		@pet[t].owner_id = @owner.id 
   	end
 
 	if (@owner.valid? && (@pet.all? { |t| t.valid? }))
@@ -22,6 +27,7 @@ class RecordsController < ApplicationController
   	  flash[:success] = "Data saved successfully"
   	  redirect_to @user  #FIX THIS
     else
+      @pet_storage = @pet
       render 'new'
     end
   end
