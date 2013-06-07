64b80c0a2b346915fd24aa5108acc0aad046c753
David R. Hemmat <dhemmat@gmail.com>
2013-06-06 21:31:47 -0400

    switched from coffee to js to avoid errors

---
+++ app/assets/javascripts/records.js
@@ -0,0 +1,19 @@
+/* #Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/ */
+
+function show_pet_forms(pets_selected, pets_allowed){
+  
+  for (var i=1;i<=pets_selected;i++)
+  { 
+    element_id = "pet-" + i;
+    document.getElementById(element_id).style.visibility = "visible";
+      document.getElementById(element_id).style.display = "block";
+  }
+  for (var i=(parseInt(pets_selected)+1);i<=pets_allowed;i++)
+  { 
+    element_id = "pet-" + i;
+    document.getElementById(element_id).style.visibility = "hidden";
+      document.getElementById(element_id).style.display = "none";
+  }
+}
